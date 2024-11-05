5c6e75400a01a8e462cdc7b09af289ec3d2244e7
RackotRR <rackotrr@gmail.com>
2024-11-05 23:47:54 +0300

    SPH2D v3.0.20 - SPH2D: add support for Rayleigh method solitary waves generation - SPH2D: support both first- and second- order dynamic NWM - SPH2D_PARAMS: add Rayleigh method solitary NWM option - SPH2D_PARAMS: add second-order dynamic NWM option

--- sources/SPH2D_OMP/src/WaveMaker.cpp
+++ sources/SPH2D_OMP/src/WaveMaker.cpp
@@ -13,21 +13,18 @@ void make_waves(
 	printlog_debug()(__func__)();
 
 	switch (params.nwm) {
-	case 1:
-		rzm_generator(r, a, nfluid, time);
-		break;
-
-	case 2:
+	case NWM_METHOD_DYNAMIC_1:
+	case NWM_METHOD_DYNAMIC_2:
 		dynamic_boundaries(r, v, nfluid, ntotal, time);
 		break;
 
-	case 3:
-		impulse_nwm(r, a, nfluid, ntotal, time);
-		break;
-
-	case 4:
+	case NWM_METHOD_WALL_DISAPPEAR:
 		disappear_wall(itype, nfluid, ntotal, time);
 		params.nwm = NWM_NO_WAVES;
+		break;
+
+	case NWM_METHOD_SOLITARY_RAYLEIGH:
+		nwm_solitary_rayleigh(r, v, nfluid, ntotal, time);
 		break;
 	default:
 		break;
@@ -63,13 +60,17 @@ void dynamic_boundaries(
 	rr_float kd = params.nwm_wave_number * params.depth;
 	rr_float m1 = 2 * sqr(sinh(kd)) / (sinh(kd) * cosh(kd) + kd);
 	rr_float S0 = H / m1;
-	rr_float e2_1 = sqr(H) / (32 * params.depth);
-	rr_float e2_2 = 3 * cosh(kd) / cube(sinh(kd)) - 2 / m1;
-	rr_float e2_coef = e2_1 + e2_2;
 
 	rr_float vx_first_order = 0.5 * S0 * omega * cos(omega * time + delta);
-	rr_float vx_second_order = 2 * omega * e2_coef * cos(2 * omega * time + 2 * delta);
-	rr_float vx = vx_first_order + vx_second_order;
+	rr_float vx = vx_first_order;
+
+	if (params.nwm == NWM_METHOD_DYNAMIC_2) {
+		rr_float e2_1 = sqr(H) / (32 * params.depth);
+		rr_float e2_2 = 3 * cosh(kd) / cube(sinh(kd)) - 2 / m1;
+		rr_float e2_coef = e2_1 + e2_2;
+		rr_float vx_second_order = 2 * omega * e2_coef * cos(2 * omega * time + 2 * delta);
+		vx += vx_second_order;
+	}
 
 	for (rr_uint i = params.nwm_particles_start; i < params.nwm_particles_end; i++) {
 		r(i).x = r(i).x + vx * params.dt;
@@ -78,6 +79,58 @@ void dynamic_boundaries(
 
 	printlog_trace("r.x: ")(r(params.nwm_particles_start).x)();
 	printlog_trace("v.x: ")(vx)();
+}
+
+void nwm_solitary_rayleigh(
+	heap_darray<rr_float2>& r,
+	heap_darray<rr_float2>& v,
+	const rr_uint nfluid,
+	const rr_uint ntotal,
+	const rr_float time)
+{
+	printlog_debug(__func__)();
+
+	// TODO: move check into initialization
+	if (params.nwm_particles_start < nfluid ||
+		params.nwm_particles_end < nfluid ||
+		params.nwm_particles_end < params.nwm_particles_start ||
+		params.nwm_particles_end > params.ntotal)
+	{
+		printlog_debug("Wrong dynamic boundaries parameters:")();
+		printlog_debug("nwm_particles_start: ")(params.nwm_particles_start)();
+		printlog_debug("nwm_particles_end: ")(params.nwm_particles_end)();
+		return;
+	}
+
+	// solitary waves generation, Rayleigh approximation (Dominguez, 2019)
+
+	rr_float H = params.nwm_wave_magnitude;
+	rr_float h = params.depth;
+	constexpr rr_float g = params.g;
+
+	rr_float k = sqrt(3 * H / (4 * sqr(h) * (H + h)));
+	rr_float c = sqrt(g * (H + h));
+	rr_float Tf = 2 / (k * c) * (3.8 + H / h);
+
+	rr_float t = time - params.nwm_time_start;
+	rr_float tau = k * c * (time - Tf);
+	rr_float u = tanh(tau);
+
+	rr_float top_left = k * H * (h + H - H * sqr(u));
+	rr_float top_right = 2 * k * sqr(H * u);
+	rr_float top = top_left + top_right;
+	rr_float bottom = sqr(k * (h + H - H * sqr(u)));
+	rr_float dxdu = top / bottom;
+
+	rr_float dudtau = 1 - sqr(tanh(tau));
+	rr_float dtaudt = k * c;
+
+	rr_float dxdt = dxdu * dudtau * dtaudt;
+
+	for (rr_uint i = params.nwm_particles_start; i < params.nwm_particles_end; i++) {
+		r(i).x = r(i).x + dxdt * params.dt;
+		v(i).x = dxdt;
+	}
 }
 
 void disappear_wall(
@@ -100,90 +153,5 @@ void disappear_wall(
 
 	for (rr_uint i = params.nwm_particles_start; i < params.nwm_particles_end; ++i) {
 		itype(i) = params.TYPE_NON_EXISTENT;
-	}
-}
-
-
-void impulse_nwm(
-	heap_darray<rr_float2>& r,
-	heap_darray<rr_float2>& a,
-	const rr_uint nfluid,
-	const rr_uint ntotal,
-	const rr_float time)
-{
-	constexpr rr_float delta = 2.f;
-	const rr_float nwmPos = params.nwm_wave_length * 2.f;
-	const rr_float factor = 4.f * sqr(params.pi);
-	for (rr_uint i = 0; i < nfluid; i++) {
-		rr_float x = r(i).x;
-		rr_float diff = factor * (1.f + params.nwm_wave_magnitude * sin(params.nwm_freq * time) * exp(-sqr(x - nwmPos) / sqr(delta)));
-		a(i).x = diff;
-	}
-}
-
-void rzm_generator(
-	const heap_darray<rr_float2>& r,
-	heap_darray<rr_float2>& a,
-	const rr_uint nfluid,
-	const rr_float time)
-{
-	for (rr_uint i = 0; i < nfluid; i++) {
-		rr_float x = r(i).x;
-		rr_float y = r(i).y;
-		static rr_float rzmg_x0 = params.nwm_wave_length * 0.25f;
-		static rr_float rzmg_xn = params.nwm_wave_length * 0.5f;
-		static rr_float rzmg_length = rzmg_xn - rzmg_x0;
-		static rr_float rzmg_center = (rzmg_x0 + rzmg_xn) * 0.5f;
-		if (x >= rzmg_x0 &&
-			x <= rzmg_xn) {
-			rr_float xc = (x - rzmg_center) / rzmg_length;
-			rr_float C = exp(-sqr(1.5f * params.pi* xc));//cos(params.pi * xc);
-			static rr_float H = params.nwm_wave_magnitude;
-			static rr_float O = params.nwm_freq;
-			static rr_float d = params.depth;
-			static rr_float k = params.nwm_wave_number;
-
-			rr_float dv_xt = 0.5f * H * O * O * cosh(k * y + k * d) / sinh(k * d) * sin(k * x - O * time);
-			rr_float dv_zt = -0.5f * H * O * O * sinh(k * y + k * d) / sinh(k * d) * cos(k * x - O * time);
-			a(i).x *= C * dv_xt + (1 - C);
-			a(i).y *= C * dv_zt + (1 - C);
-		}
-		else {
-			a(i) = { 0.f };
-		}
-	}
-}
-
-void rzm_absorber(
-	const heap_darray<rr_float2>& r,
-	const heap_darray<rr_float2>& v,
-	heap_darray<rr_float2>& a,
-	const rr_uint nfluid,
-	const rr_float time)
-{
-	for (rr_uint i = 0; i < nfluid; i++) {
-		rr_float x = r(i).x;
-		rr_float y = r(i).y;
-		static rr_float rzma_x0 = params.beach_x;
-		static rr_float rzma_xn = params.x_maxgeom;
-		static rr_float rzma_length = rzma_xn - rzma_x0;
-		if (x >= rzma_x0 &&
-			x <= rzma_xn) {
-			rr_float xc = (x - rzma_x0) / rzma_length;
-			rr_float C = sin(params.pi * 0.5f * (xc + 1));
-			static rr_float H = params.nwm_wave_magnitude;
-			static rr_float O = params.nwm_freq;
-			static rr_float d = params.depth;
-			static rr_float k = params.nwm_wave_number;
-			rr_float dv_xt = H * O * O * cosh(k * y + k * d) / sinh(k * d) * sin(k * x - O * time);
-			rr_float dv_zt = -H * O * O * sinh(k * y + k * d) / sinh(k * d) * cos(k * x - O * time);
-
-			rr_float au = a(i).x * v(i).x;
-			a(i).x = au > 0 ? C * dv_xt : dv_xt;
-			a(i).y = au > 0 ? C * dv_zt : dv_zt;
-		}
-		else {
-			a(i) = { 0.f };
-		}
 	}
 }
